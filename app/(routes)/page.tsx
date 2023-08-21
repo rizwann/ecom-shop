79617c9b3ecb2264f430ee36f09ef579e2c2b0b3
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-08-21 23:52:24 +0200

    ADD: home page skeleton & data fetching

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,26 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard ";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({
+    isFeatured: true,
+  });
+  const billboard = await getBillboard("bc5b5ad4-b3b4-40ea-9fdf-0886c11938fa");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" products={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
---
+++ components/product-list.tsx
@@ -0,0 +1,12 @@
+import { Product } from "@/types";
+
+interface ProductListProps {
+  title: string;
+  products: Product[];
+}
+
+const ProductList = () => {
+  return <div>ProductList</div>;
+};
+
+export default ProductList;
