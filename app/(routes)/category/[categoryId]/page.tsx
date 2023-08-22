a87b42ecabe4ccf8a402ed112f66350c9d1cd1b6
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-08-22 12:38:13 +0200

    ADD: mobile filter dialog

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from "@/components/ui/container";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
 import Filter from "./components/Filter";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -41,6 +42,7 @@ const Category: React.FC<CategoryPageProps> = async ({
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             {/* Mobile Filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
 
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
