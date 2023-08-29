9bb174708179a261cc018176eabb145cad68c798
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-08-29 23:57:47 +0200

    _

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -10,7 +10,9 @@ interface ProductListProps {
 const ProductList: React.FC<ProductListProps> = ({ title, products }) => {
   return (
     <div className="space-y-4">
-      <h3 className="font-bold text-3xl">{title}</h3>
+      <h3 className="font-bold text-3xl">
+        {title} ({products?.length})
+      </h3>
       {products.length === 0 && <NoResults />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map((product) => (
